a1a488ad3a0bf948d4ec5ede0459d6a31e489b2a
Yaroslav Zhyvotovskyi <yaroslav.zhyvotovskyi@bucher-suter.com>
2024-12-09 15:30:20 +0100

    add example and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+1. Go through notion [integration guide](https://developers.notion.com/docs/create-a-notion-integration#getting-started) and copy the API secret
+2. Create `.env` file from `.env_example`, and paste the secret there
+3. Create the page in notion and connect it to the integration (top-right 3 dots and scroll to the bottom -> Connect to -> *your integration*)
+4. id
+5. Run `node main.js`
---
+++ main.js
@@ -0,0 +1,80 @@
+const { Client } = require('@notionhq/client');
+require("dotenv").config();
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+(async () => {
+  const response = await notion.pages.create({
+    "cover": {
+        "type": "external",
+        "external": {
+            "url": "https://upload.wikimedia.org/wikipedia/commons/6/62/Tuscankale.jpg"
+        }
+    },
+    "icon": {
+        "type": "emoji",
+        "emoji": "🥬"
+    },
+    "parent": {
+        "type": "page_id",
+        "page_id": "157e770eab68808f8c9ee35009d859a8"
+    },
+    "properties": {
+        "title": {
+            "id": "title",
+            "type": "title",
+            "title": [
+                {
+                    "type": "text",
+                    "text": {
+                        "content": "Bug bash",
+                        "link": null
+                    },
+                    "annotations": {
+                        "bold": false,
+                        "italic": false,
+                        "strikethrough": false,
+                        "underline": false,
+                        "code": false,
+                        "color": "default"
+                    },
+                    "plain_text": "Bug bash",
+                    "href": null
+                }
+            ]
+        }
+    },
+    "children": [
+        {
+            "object": "block",
+            "heading_2": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": "Lacinato kale"
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            "object": "block",
+            "paragraph": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
+                            "link": {
+                                "url": "https://en.wikipedia.org/wiki/Lacinato_kale"
+                            }
+                        },
+                        "href": "https://en.wikipedia.org/wiki/Lacinato_kale"
+                    }
+                ],
+                "color": "default"
+            }
+        }
+    ]
+});
+  console.log(response);
+})();
