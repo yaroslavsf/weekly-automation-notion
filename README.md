a31d6f06feda3510b4494210bc1de44d37a08bc8
Yaroslav Zhyvotovskyi <yaroslav.zhyvotovskyi@bucher-suter.com>
2024-12-10 08:52:47 +0100

    add script

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-1. Go through notion [integration guide](https://developers.notion.com/docs/create-a-notion-integration#getting-started) and copy the API secret
-2. Create `.env` file from `.env_example`, and paste the secret there
-3. Create the page in notion and connect it to the integration (top-right 3 dots and scroll to the bottom -> Connect to -> *your integration*)
-4. id
+1. Go through [notion integration guide](https://developers.notion.com/docs/create-a-notion-integration#getting-started) and get the API secret
+2. Create the page in notion and get the ID of the page (share page -> get ID from URL)
+2. Create `.env` file from `.env_example`, and set the secret and page ID
+3. Connect the notion page to the integration (top-right 3 dots and scroll to the bottom -> Connect to -> *your integration*)
 5. Run `node main.js`
--- main.js
+++ main.js
@@ -1,80 +1,269 @@
 const { Client } = require('@notionhq/client');
+const moment = require('moment');
 require("dotenv").config();
 
+const prompt = require("prompt-sync")({ sigint: true });
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const parent_page_id = process.env.PARENT_PAGE_ID
 
-(async () => {
-  const response = await notion.pages.create({
-    "cover": {
-        "type": "external",
-        "external": {
-            "url": "https://upload.wikimedia.org/wikipedia/commons/6/62/Tuscankale.jpg"
-        }
-    },
-    "icon": {
-        "type": "emoji",
-        "emoji": "🥬"
-    },
-    "parent": {
-        "type": "page_id",
-        "page_id": "157e770eab68808f8c9ee35009d859a8"
-    },
-    "properties": {
-        "title": {
-            "id": "title",
-            "type": "title",
-            "title": [
-                {
-                    "type": "text",
-                    "text": {
-                        "content": "Bug bash",
-                        "link": null
-                    },
-                    "annotations": {
-                        "bold": false,
-                        "italic": false,
-                        "strikethrough": false,
-                        "underline": false,
-                        "code": false,
-                        "color": "default"
-                    },
-                    "plain_text": "Bug bash",
-                    "href": null
-                }
-            ]
-        }
-    },
-    "children": [
-        {
-            "object": "block",
-            "heading_2": {
-                "rich_text": [
-                    {
-                        "text": {
-                            "content": "Lacinato kale"
-                        }
-                    }
-                ]
+const firstname = prompt("What is your first name? ");
+const lastname = prompt("What is your last name? ");
+const year = prompt("What year? (yy) ");
+const week_from = prompt("Starting from what week? ");
+const week_to = 52; // Alblak?
+
+async function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+createEmptyPage(notion, parent_page_id, year)
+    .then(async (response) => {
+        const toggle_id = response.id
+        for (let i = week_from; i <= week_to; i++) {
+            await createWeekPage(notion, toggle_id, i, year, firstname, lastname)
+            if (i % 3 === 0) {
+                await delay(1000);  // 1 second delay
             }
+        }
+    })
+    .catch((e) => {
+        console.log(e)
+    })
+
+
+
+
+async function createEmptyPage(notion, parentBlockId, year) {
+
+    return notion.pages.create({
+        parent: {
+            type: "page_id",
+            page_id: parentBlockId
+        },  // Set the parent database ID
+        properties: {
+            'title': { title: [{ text: { content: `${year}-${parseInt(year, 10) + 1}` } }] },
         },
-        {
-            "object": "block",
-            "paragraph": {
-                "rich_text": [
-                    {
-                        "text": {
-                            "content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
-                            "link": {
-                                "url": "https://en.wikipedia.org/wiki/Lacinato_kale"
+    });
+}
+
+async function createWeekPage(notion, parentBlockId, weekNumber, year, firstname, lastname) {
+    // Calculate the first day of the week (Monday)
+    const firstDayOfWeek = moment().year(year).week(weekNumber).startOf('isoWeek');
+
+    // Create a new page in Notion for the calendar week
+    const page = await notion.pages.create({
+        parent: {
+            type: "page_id",
+            page_id: parentBlockId
+        },  // Set the parent database ID
+        properties: {
+            title: { title: [{ text: { content: `Arbeitsjournal-${firstname}-${lastname}-20${year}-${weekNumber}` } }] },
+        },
+    });
+
+    const pageId = page.id;
+
+    // Add a table for the week
+    await notion.blocks.children.append({
+        block_id: pageId,
+        children: [
+            {
+                type: "divider",
+                divider: {}
+            },
+            {
+                type: 'column_list',
+                column_list: {
+                    children: [
+                        {
+                            type: 'column',
+                            column: {
+                                children: [
+                                    {
+                                        object: "block",
+                                        type: "paragraph",
+                                        paragraph: {
+                                            rich_text: [
+                                                {
+                                                    type: "text",
+                                                    text: {
+                                                        content: "Documentation"
+                                                    },
+                                                    annotations: {
+                                                        bold: true,
+                                                    },
+                                                }
+                                            ]
+                                        }
+                                    }
+                                ],
                             }
                         },
-                        "href": "https://en.wikipedia.org/wiki/Lacinato_kale"
-                    }
-                ],
-                "color": "default"
+                        {
+                            type: 'column',
+                            column: {
+                                children: [
+                                    {
+                                        object: "block",
+                                        type: "paragraph",
+                                        paragraph: {
+                                            rich_text: [
+                                                {
+                                                    type: "text",
+                                                    text: {
+                                                        content: `CW ${weekNumber} / 20${year}`
+                                                    },
+                                                    annotations: {
+                                                        bold: true,
+                                                    },
+                                                }
+                                            ]
+                                        }
+                                    }
+                                ],
+                            }
+
+                        },
+                        {
+                            type: 'column',
+                            column: {
+                                children: [
+                                    {
+                                        object: "block",
+                                        type: "paragraph",
+                                        paragraph: {
+                                            rich_text: [
+                                                {
+                                                    type: "text",
+                                                    text: {
+                                                        content: `${firstname} ${lastname}`
+                                                    },
+                                                    annotations: {
+                                                        bold: true,
+                                                    },
+                                                }
+                                            ]
+                                        }
+                                    }
+                                ],
+                            }
+
+                        },
+                    ]
+                },
+            },
+            {
+                type: "divider",
+                divider: {}
+            },
+            {
+                object: 'block',
+                type: 'equation',
+                equation: {
+                    expression: '\\large Activities',
+                },
+            },
+            {
+                object: 'block',
+                type: 'table',
+                table: {
+                    table_width: 3,
+                    has_column_header: true,
+                    children: [
+                        {
+                            object: 'block',
+                            type: 'table_row',
+                            table_row: {
+                                cells: [
+                                    [{ type: 'text', text: { content: 'Day, Place, Date' } }],
+                                    [{ type: 'text', text: { content: 'Activities' } }],
+                                    [{ type: 'text', text: { content: 'Time in h' } }],
+                                ],
+                            },
+                        },
+                        ...Array.from({ length: 5 }, (_, i) => {
+                            const dayOfWeek = firstDayOfWeek.clone().add(i, 'days');
+                            return {
+                                object: 'block',
+                                type: 'table_row',
+                                table_row: {
+                                    cells: [
+                                        [{ type: 'text', text: { content: `${dayOfWeek.format('dddd')}, NY, ${dayOfWeek.format('DD.MM.YY')}` } }],
+                                        [{ type: 'text', text: { content: '-' } }],
+                                        [{ type: 'text', text: { content: '8.4' } }],
+                                    ],
+                                },
+                            };
+                        }),
+                    ],
+                },
+            },
+            // Block for the 3 equations
+            {
+                object: 'block',
+                type: 'equation',
+                equation: {
+                    expression: '\\large Weekly~review',
+                },
+            },
+            {
+                object: "block",
+                type: "paragraph",
+                paragraph: {
+                    rich_text: [
+                        {
+                            type: "text",
+                            text: {
+                                content: "tbd"
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: 'equation',
+                equation: {
+                    expression: '\\large Reflection',
+                },
+            },
+            {
+                object: "block",
+                type: "paragraph",
+                paragraph: {
+                    rich_text: [
+                        {
+                            type: "text",
+                            text: {
+                                content: "tbd"
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: 'equation',
+                equation: {
+                    expression: '\\large Mood~of~the~week',
+                },
+            },
+            {
+                object: "block",
+                type: "paragraph",
+                paragraph: {
+                    rich_text: [
+                        {
+                            type: "text",
+                            text: {
+                                content: "tbd"
+                            }
+                        }
+                    ]
+                }
             }
-        }
-    ]
-});
-  console.log(response);
-})();
+        ],
+    });
+
+    console.log(`Week ${weekNumber} / 52 created`);
+}
